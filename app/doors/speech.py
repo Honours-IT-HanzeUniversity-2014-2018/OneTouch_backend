02bfd1f47de0550b2b831ae0c0dce412a1056078
Robert de Vries <r.devries@robbytu.net>
2015-08-27 21:49:48 +0200

    + Implemented the speech processor for the doors app

--- app/doors/speech.py
+++ app/doors/speech.py
@@ -1,2 +1,17 @@
+from .models import Door
+
+
 def process_speech(command):
-    return command == 'test'
+    # This one is kind of simple. We only 'toggle' the doors status, so it can either open or close, but the
+    # behaviour is actually determined by reading from the database. Therefor, it does not really matter all that much
+    # what the user said. We may assume they want to open or close a door, as long as the command includes a name of one
+    # of the doors in the database. As the status of these doors is being saved in the database, we can simply call the
+    # toggle() definition on the database entry object.
+
+    words = command.split(' ')
+    matches = Door.objects.filter(name__in=words)
+
+    for match in matches:
+        match.toggle()
+
+    return len(matches) > 0
