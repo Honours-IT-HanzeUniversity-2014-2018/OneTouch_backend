0746e8d0f4bbe1cff5f5c6dac6773e20501d531f
R. de Vries <r.devries@robbytu.net>
2015-08-27 07:48:47 +0000

    + Implemented dynamic doors view

--- app/doors/views.py
+++ app/doors/views.py
@@ -1,11 +1,14 @@
 from utils import JsonMenuView, MenuItem
-
+from .models import Door
 
 class MainMenu(JsonMenuView):
     title = 'Deuren'
 
     def getMenu(self):
         return [
-            MenuItem('Voordeur', icon="key"),
-            MenuItem('Achterdeur', icon="key"),
+            MenuItem(door.name,
+                     icon='key',
+                     statuses=[['on', 'off'][int(door.status)]],
+                     action='/blaat'
+             ) for door in Door.objects.all()
         ]
