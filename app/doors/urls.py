890f20cbfbac170a346fb0939d7bf15d2873317d
R. de Vries <r.devries@robbytu.net>
2015-08-27 08:12:59 +0000

    + Added urldef for door_toggle

--- app/doors/urls.py
+++ app/doors/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^menu.json$', views.MainMenu.as_view(), name='main_menu'),
+    url(r'^toggle/(?P<pk>[\d]+).json$', views.ToggleDoor.as_view(), name='toggle_door'),
 ]
