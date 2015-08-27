324a1b266d08de56b1df52bf684936b85640be0a
R. de Vries <r.devries@robbytu.net>
2015-08-27 08:25:19 +0000

    * DRY principle, refactored doors.views

--- app/doors/views.py
+++ app/doors/views.py
@@ -3,24 +3,22 @@ from django.core.urlresolvers import reverse
 from utils import JsonMenuView, JsonItemView, MenuItem
 from .models import Door
 
+class DoorMenuItem(MenuItem):
+    def __init__(self, door):
+        self.name = door.name
+        self.icon = 'key'
+        self.statuses = [['status-on', 'status-off'][int(door.status)]]
+        self.action = reverse('doors:toggle_door', kwargs={'pk': door.pk})
+
 class MainMenu(JsonMenuView):
     title = 'Deuren'
 
     def getMenu(self):
-        return [
-            MenuItem(door.name,
-                     icon='key',
-                     statuses=[['on', 'off'][int(door.status)]],
-                     action=reverse('doors:toggle_door', kwargs={'pk': door.pk})
-             ) for door in Door.objects.all()
-        ]
+        return [DoorMenuItem(door) for door in Door.objects.all()]
 
 class ToggleDoor(JsonItemView):
     def getMenu(self):
         door = get_object_or_404(Door, pk=self.kwargs['pk'])
         door.toggle()
 
-        return MenuItem(door.name,
-                        icon='key',
-                        statuses=[['on', 'off'][int(door.status)]],
-                        action=reverse('doors:toggle_door', kwargs={'pk': door.pk}))
+        return DoorMenuItem(door)
