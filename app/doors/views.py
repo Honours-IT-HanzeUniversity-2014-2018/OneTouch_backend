5692800ef569c04918bd7c58b298e09794286a9b
R. de Vries <r.devries@robbytu.net>
2015-08-27 08:14:06 +0000

    + Implemented the door_toggle view

--- app/doors/views.py
+++ app/doors/views.py
@@ -1,4 +1,6 @@
-from utils import JsonMenuView, MenuItem
+from django.shortcuts import get_object_or_404
+from django.core.urlresolvers import reverse
+from utils import JsonMenuView, JsonItemView, MenuItem
 from .models import Door
 
 class MainMenu(JsonMenuView):
@@ -9,6 +11,16 @@ class MainMenu(JsonMenuView):
             MenuItem(door.name,
                      icon='key',
                      statuses=[['on', 'off'][int(door.status)]],
-                     action='/blaat'
+                     action=reverse('doors:toggle_door', kwargs={'pk': door.pk})
              ) for door in Door.objects.all()
         ]
+
+class ToggleDoor(JsonItemView):
+    def getMenu(self):
+        door = get_object_or_404(Door, pk=self.kwargs['pk'])
+        door.toggle()
+
+        return MenuItem(door.name,
+                        icon='key',
+                        statuses=[['on', 'off'][int(door.status)]],
+                        action=reverse('doors:toggle_door', kwargs={'pk': door.pk}))
